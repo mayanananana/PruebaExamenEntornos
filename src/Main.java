6ef0fedf5db041de26d200f0c0e8ea1afcdddd7e
Mariana <marianagomezdam@gmail.com>
2025-05-20 11:35:49 +0200

    version inicial

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido al programa :)");
-            System.out.println("Te quiero");
+            System.out.println("Te quiero mucho");
 
         for (int i = 0; i < 3; i++) {
             System.out.println("*********************");
