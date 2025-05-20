c0343f7d57d90694bcb81842820208d5867da658
Mariana <marianagomezdam@gmail.com>
2025-05-20 11:22:55 +0200

    version inicial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al programa :)");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("*********************");
+            for (int j = 0; j < 3; j++) {
+
+                System.out.println("*                   *");
+                System.out.println("*                   *");
+                System.out.println("*                   *");
+                System.out.println("*                   *");
+                System.out.println("*                   *");
+                System.out.println("*********************");
+
+
+            }
+
+        }
+    }
+}
