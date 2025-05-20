0e1f2a0834500a552cb7b6780b26296fd1950527
mayanananana <marianagomezdam@gmail.com>
2025-05-20 11:35:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido al programa :)");
-            System.out.println("Te odio");
+            System.out.println("Te odio mucho");
 
         for (int i = 0; i < 3; i++) {
             System.out.println("*********************");
